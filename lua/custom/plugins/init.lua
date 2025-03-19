212aaccd023ef42fe0efa267ad4f43b8eb31d350
Elias Landin <elias.landin@me.com>
2025-03-19 16:03:44 +0100

    Add coloriize pluigin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup({
+        'css',
+        'javascript',
+        'html',
+      }, { css = true, mode = 'foreground' })
+    end,
+  },
+}
