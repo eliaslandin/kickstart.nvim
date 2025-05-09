73caea1c75d5e84f320b8fd8cc1a888cfb4bba11
Elias Landin <elias.landin@me.com>
2025-05-09 16:04:54 +0200

    Add dadbod completion to nvim-cmp

--- init.lua
+++ init.lua
@@ -884,6 +884,13 @@ require('lazy').setup({
           { name = 'path' },
         },
       }
+
+      cmp.setup.filetype({ 'sql' }, {
+        sources = {
+          { name = 'vim-dadbod-completion' },
+          { name = 'buffer' },
+        },
+      })
     end,
   },
 
